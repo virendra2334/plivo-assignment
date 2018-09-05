170d66a2763a0b3f2c7fc229861a2f0e45548a73
Virendra <virendra2334@gmail.com>
2018-09-05 14:18:26 +0530

    Add install scripts

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-
 #Installing apt packages first
 sudo apt-get install -y postgresql
 
-sudo apt-get install -y git
-
-#Install pip packages
-sudo pip install -r requirements.txt
+#Installing redis server
+sudo apt-get install -y redis-server
 
 #Setup Postgres
 ./setup_psql.sh
+
+mkdir venv && cd venv && virtualenv assignment and source assignment/bin/activate
+
+#Install pip packages
+pip install -r requirements.txt
+
+cd assignment && ./manage.py migrate && ./manage.py loaddata initial_data
--- setup/setup_psql.sh
+++ setup/setup_psql.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+prev_user=$USER
+sudo -s
+su postgres
+su prev_user
+
+sudo mv /etc/postgresql/10/main/pg_hba.conf /etc/postgresql/10/main/pg_hba_default.conf
+sudo cp ../config/pg_hba.conf /etc/postgresql/10/main/
+sudo service postgresql restart
+
